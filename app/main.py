460a7dc612973ba6d3c7760697409363989bf74f
green-thunder <kamolovnodirbek1@gmail.com>
2023-11-05 21:27:48 +0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
-
+    sock, addr = server_socket.accept()  # wait for client
+    data = sock.recv(1024)
+    sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
