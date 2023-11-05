a216d87866e4913a13fb655cde84de6400a19d4a
green-thunder <kamolovnodirbek1@gmail.com>
2023-11-05 21:37:17 +0500

    stage 3

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 import socket
 
+def getPath(data):
+
+    decoded_data = data.decode()
+    first_line = decoded_data.split("\r\n")[0]
+    path = first_line.split(" ")[1]
+
+    return path
 
 def main():
-    print("Logs from your program will appear here!")
-
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     sock, addr = server_socket.accept()  # wait for client
     data = sock.recv(1024)
-    sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    path = getPath(data)
+    
+    if path == "/":
+        sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        sock.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
