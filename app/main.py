3491fb1dc8edbd023850c243f659a8728688839e
green-thunder <kamolovnodirbek1@gmail.com>
2023-11-05 21:30:42 +0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def main():
 
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
     sock, addr = server_socket.accept()  # wait for client
     data = sock.recv(1024)
     sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
